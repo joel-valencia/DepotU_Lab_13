ecb288b3aed5f7bd5901340440607b59025369b5
Joel Valencia <trekkerj1@gmail.com>
2016-03-15 15:51:07 -0500

    html for tweet composer.  only enable buttons when needed

--- client/js/main.js
+++ client/js/main.js
@@ -4,6 +4,15 @@ angular.module('myApp', ['ngRoute'])
         $rootScope.logIn = function() {
             location.hash = "#/tweets";
         };
+        $('#login').prop('disabled', true);
+        
+        $('#username').keyup(function() {
+            if ($('#username').val() == "") {
+                $('#login').prop('disabled', true);
+            } else {
+                $('#login').prop('disabled', false);
+            }
+        });
 	}])
 	.controller('tweetsController', ['$rootScope', function($rootScope){
         $rootScope.getData = function() {
@@ -11,15 +20,24 @@ angular.module('myApp', ['ngRoute'])
                 url: 'http://localhost:3000/messages',
                 method: 'GET'
             }).then(function(result) {
-                
-                
                 $rootScope.tweets = result;
-                
                 console.log($rootScope.tweets);
             });
         }
         
         $rootScope.getData();
+        
+        $('#send').prop('disabled', true);
+                
+        $('#compose').keyup(function() {
+            if ($('#compose').val() == "") {
+                $('#send').prop('disabled', true);
+            } else {
+                $('#send').prop('disabled', false);
+            }
+        });
+                
+                
 	}])
 	.config(['$routeProvider', function($routeProvider){
         $routeProvider
